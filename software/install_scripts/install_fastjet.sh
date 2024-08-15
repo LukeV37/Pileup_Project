2bc60746acb54136e6a106084fdb364341008bdd
lvaughan <luke.vaughan@okstate.edu>
2024-08-15 12:30:57 -0500

    software

--- software/install_scripts/install_fastjet.sh
+++ software/install_scripts/install_fastjet.sh
@@ -2,5 +2,7 @@
 cd ../FastJet-3.4.2
 mkdir fastjet-install
 ./autogen.sh --prefix="$PWD/fastjet-install/"
+./autogen.sh --prefix="$PWD/fastjet-install/"
 make -j4
 make install
+rm -f ../../ltmain.sh
