8c1dbaf93ef94005b6464fa16567bdf8473777f6
lvaughan <luke.vaughan@okstate.edu>
2024-08-14 23:38:20 -0500

    delphes

--- delphes/run_ATLAS_sim.sh
+++ delphes/run_ATLAS_sim.sh
@@ -3,6 +3,6 @@ dir=$PWD
 rm -f output/dataset.root
 cd ../software/Delphes-3.5.0
 ./DelphesHepMC2 ../../delphes/okstate_card_ATLAS_PileUp.tcl ../../delphes/output/dataset.root ../../pythia/output/ttbar.hepmc
-./DelphesHepMC2 delphes_card_ATLAS_PileUp.tcl ../../delphes/output/dataset.root ../../pythia/output/ttbar.hepmc
+#./DelphesHepMC2 delphes_card_ATLAS_PileUp.tcl ../../delphes/output/dataset.root ../../pythia/output/ttbar.hepmc
 cd $dir
 #../software/Delphes-3.5.0/hepmc2pileup TEST.PILEUP ../pythia/output/pileup.hepmc
