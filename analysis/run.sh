9a963327dbfd87c3f15d2e6e284276e418c9256a
lvaughan <luke.vaughan@okstate.edu>
2024-08-19 14:17:54 -0500

    run root scripts

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-cd src
-root -q -l add_true_pufr.C
-root -q -l add_ttbar_match.C
+for entry in ntuples/*
+do
+    echo "Processing: $entry"
+    mv $entry ./ntuples/dataset.root
+    cd src
+    root -q -l -b add_true_pufr.C
+    root -q -l -b add_ttbar_match.C
+    cd ..
+    mv ./ntuples/dataset.root $entry
+    echo
+done
