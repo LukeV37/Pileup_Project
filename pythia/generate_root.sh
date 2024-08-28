d2959ef8c60a7eb5a46de1f972266fc5b341e6c3
Luke Martin Vaughan <luke.martin.vaughan@cern.ch>
2024-08-27 22:05:59 -0500

    add arg checker

--- pythia/generate_root.sh
+++ pythia/generate_root.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ -z "$4" ]; then
+    echo "Must enter 4 agruments"
+    echo "1: Num Events (int)"
+    echo "2: Average PU, mu, (int)"
+    echo "3: Process {ttbar|zprime}"
+    echo "4: MinJetpT (float)"
+    exit 1
+fi
+
 cd src
 make generate_root
 ./run_root $1 $2 $3 $4
