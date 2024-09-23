3768292b4e44d3785df0eab5423ac66f827febe0
lvaughan <luke.vaughan@okstate.edu>
2024-09-23 16:32:37 -0500

    4b pT Filter

--- madgraph/run_4b.sh
+++ madgraph/run_4b.sh
@@ -7,6 +7,8 @@ Beam_Energy=7000.0
 sed -i "s/6500.0/$Beam_Energy/" 4b/Cards/run_card.dat
 num_Events=10000
 sed -i "s/\(.*\)= nevents\(.*\)/ $num_Events = nevents\2/" 4b/Cards/run_card.dat
+# Filter mininum pt of b quark
+#sed -i "s/0.0  = ptb/20.0  = ptb/" 4b/Cards/run_card.dat
 
 # Generate LHE File
 4b/bin/generate_events
