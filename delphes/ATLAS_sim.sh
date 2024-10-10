755268d96ed28614b02160c62660d95b68ae7837
lvaughan <luke.vaughan@okstate.edu>
2024-10-10 18:34:37 -0500

    rm tcl file extension

--- delphes/ATLAS_sim.sh
+++ delphes/ATLAS_sim.sh
@@ -2,6 +2,6 @@
 dir=$PWD
 rm -f output/dataset.root
 cd ../software/Delphes-3.5.0
-./DelphesHepMC2 ../../delphes/okstate_card_ATLAS_PileUp.tcl ../../delphes/output/dataset.root ../../pythia/output/ttbar.hepmc
-#./DelphesHepMC2 delphes_card_ATLAS_PileUp.tcl ../../delphes/output/dataset.root ../../pythia/output/ttbar.hepmc
+./DelphesHepMC2 ../../delphes/okstate_card_ATLAS_PileUp.config ../../delphes/output/dataset.root ../../pythia/output/ttbar.hepmc
+#./DelphesHepMC2 delphes_card_ATLAS_PileUp.config ../../delphes/output/dataset.root ../../pythia/output/ttbar.hepmc
 cd $dir
