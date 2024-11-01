e0c8780ab755e33df2d042e877da2824a0bce66a
lvaughan <luke.vaughan@okstate.edu>
2024-11-01 16:20:35 -0500

    add seed

--- madgraph/run_dihiggs.sh
+++ madgraph/run_dihiggs.sh
@@ -4,11 +4,13 @@ cd output
 
 # Edit Run Card
 Beam_Energy=7000.0
-num_Events=50000
+num_Events=20000
+seed=1234
 
 # Apply changes to run card
 sed -i "s/6500.0/$Beam_Energy/" DiHiggs/Cards/run_card.dat
 sed -i "s/\(.*\)= nevents\(.*\)/ $num_Events = nevents\2/" DiHiggs/Cards/run_card.dat
+sed -i "s/\(.*\)= iseed\(.*\)/ $seed = iseed\2/" DiHiggs/Cards/run_card.dat
 
 # Generate LHE File
 DiHiggs/bin/generate_events
